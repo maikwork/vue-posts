a503d2812447f528f8eba7f9ef76b676ab839bff
malonchako <malonchako@mts.ru>
2022-09-02 22:45:03 +0300

    Move form to componet PostForm

---
+++ src/components/PostForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <form @submit.prevent>
+    <input 
+    v-bind:value="title"
+    @input="title = $event.target.value"
+    type="text" 
+    class="input" 
+    placeholder="Название">
+    <input 
+    v-bind:value="text"
+    @input="text = $event.target.value"
+    type="text" 
+    class="input" 
+    placeholder="Описание">
+    <button @click="createPost" class="btn">Добавить</button>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
