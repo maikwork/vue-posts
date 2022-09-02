a8d91b2bff9752350f0e5c5280c67556fdbb2670
malonchako <malonchako@mts.ru>
2022-09-02 22:49:12 +0300

    Move list to component PostList

---
+++ src/components/PostList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="post" v-bind:key="post.id" v-for="post in posts">
+    <div>Название: <strong>{{post.title}}</strong></div>
+    <div>Описание: <strong>{{post.text}}</strong></div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        posts: {
+            type: Array, 
+            required: true,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
